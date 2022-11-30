5beaddbbd47bc150d141e42577e0ba21bdfb6c1f
JJMUNPER <jmunper007@g.educaand.es>
2022-11-30 12:57:55 +0100

    Ejercicio finalizado

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  num1:number=0;
+  num2:number=0;
+  resultado:number=0;
+
+  constructor(){}
+    suma(){
+      this.resultado=this.num1 + this.num2;
+    }
+
+    resta(){
+      this.resultado=this.num1 - this.num2;
+    }
+
+    multiplicacion(){
+      this.resultado=this.num1 * this.num2;
+    }
+
+    division(){
+      this.resultado=this.num1 / this.num2;
+    }
+
+    reinicio(){
+      this.num1=0;
+      this.num2=0;
+      this.resultado=0;
+
+    }
+}
+
